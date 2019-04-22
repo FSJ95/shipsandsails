79b111976dc19b7863444945dd3e4c58b4c82c89
oliverlowbob <oliv605m@stud.kea.dk>
2019-04-22 15:12:19 +0200

    så der guf gutter

--- Game/src/main/java/dk/kea/shipgame/Repository/MoveRepo.java
+++ Game/src/main/java/dk/kea/shipgame/Repository/MoveRepo.java
@@ -3,14 +3,17 @@ import dk.kea.shipgame.Model.*;
 import dk.kea.shipgame.Service.IMove;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class MoveRepo implements IMove {
 
     @Autowired
     JdbcTemplate template;
 
+    List<Coordinate> coordinateList;
 
     @Override
     public void movement(List<Ship> ships, List<Order> orders, Weather weather) {
@@ -20,10 +23,8 @@ public class MoveRepo implements IMove {
                 0, 100, 100, 100, 0, 0);
         ships.add(ship1);
 
-        List<Coordinate> coordinateList = new List<Coordinate>();
-    }
+        Order order = new Order(1, 1, 1, 2, coordinateList, true, false, 1, 1);
 
-        Order order = new Order(1, 1, 1, 2, coordinateList, true, false, 1, int target) {
 
 
         }
