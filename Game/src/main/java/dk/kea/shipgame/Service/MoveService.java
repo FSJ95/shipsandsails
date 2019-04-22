a3dc4d9b4e492b6556a24a3ffe7265e006db80bf
oliverlowbob <oliv605m@stud.kea.dk>
2019-04-22 13:50:20 +0200

    moverepo moveservice create

---
+++ Game/src/main/java/dk/kea/shipgame/Service/MoveService.java
@@ -0,0 +1,22 @@
+package dk.kea.shipgame.Service;
+
+import dk.kea.shipgame.Model.Order;
+import dk.kea.shipgame.Model.Ship;
+import dk.kea.shipgame.Model.Weather;
+import dk.kea.shipgame.Repository.MoveRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MoveService {
+
+    @Autowired
+    MoveRepo moveRepo;
+
+    public void movement(List<Ship> ships, List<Order> orders, Weather weather){
+        moveRepo.movement(ships, orders, weather);
+    }
+
+}
