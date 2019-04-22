73cb466147a608920ab2d4ca89304562bae4436c
oliverlowbob <oliv605m@stud.kea.dk>
2019-04-22 15:04:51 +0200

    updater med testskib heh hygge order virker ik doe

--- Game/src/main/java/dk/kea/shipgame/Repository/MoveRepo.java
+++ Game/src/main/java/dk/kea/shipgame/Repository/MoveRepo.java
@@ -1,7 +1,5 @@
 package dk.kea.shipgame.Repository;
-import dk.kea.shipgame.Model.Order;
-import dk.kea.shipgame.Model.Ship;
-import dk.kea.shipgame.Model.Weather;
+import dk.kea.shipgame.Model.*;
 import dk.kea.shipgame.Service.IMove;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,5 +15,16 @@ public class MoveRepo implements IMove {
     @Override
     public void movement(List<Ship> ships, List<Order> orders, Weather weather) {
 
+        Coordinate coordinate = new Coordinate(5,5);
+        Ship ship1 = new Ship(1, 1, "Danmark", "Kaptajn Boosted", coordinate, Direction.SW,
+                0, 100, 100, 100, 0, 0);
+        ships.add(ship1);
+
+        List<Coordinate> coordinateList = new List<Coordinate>();
     }
+
+        Order order = new Order(1, 1, 1, 2, coordinateList, true, false, 1, int target) {
+
+
+        }
 }
