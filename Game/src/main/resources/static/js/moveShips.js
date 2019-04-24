b6fe9cffdaba23191ddf769c272ef817f513e034
Tobias Midskard <tobiasmidskard@tobiass-MacBook-Pro.local>
2019-04-24 18:45:03 +0200

    shipstats

--- Game/src/main/resources/static/js/moveShips.js
+++ Game/src/main/resources/static/js/moveShips.js
@@ -3,6 +3,52 @@ let chosenShip = null;
 function initalizeMove(ship) {
     chosenShip = ship;
     console.log(chosenShip);
+    changeStatsView(ship)
+
+}
+
+function changeStatsView(ship) {
+    let shiptype = document.getElementById("statsshiptype");
+
+    let shiptypeName = "";
+
+    switch (ship.shipType) {
+        case 1:
+            shiptypeName = "Brig";
+            break;
+        case 2:
+            shiptypeName = "Ship of the line";
+            break;
+        case 3:
+            shiptypeName = "Man at war";
+            break;
+    }
+
+    shiptype.innerText = shiptypeName;
+
+    let shipimage = document.getElementById("statsshipimage");
+
+    switch (ship.nationality.country) {
+        case "Denmark":
+            shipimage.src = '../img/ships/brig/red.png';
+            break;
+        case "Japan":
+            shipimage.src = '../img/ships/brig/white.png';
+            break;
+        case "Argentina":
+            shipimage.src = '../img/ships/brig/teal.png';
+            break;
+        case "Portugal":
+            shipimage.src = '../img/ships/brig/green.png';
+            break;
+    }
+
+    let country = document.getElementById("statscountry");
+    country.innerText = ship.nationality.country;
+
+    let captain = document.getElementById("statscaptain");
+    captain.innerText = ship.nationality.captain;
+
 }
 
 function finalizeMove(tile) {
